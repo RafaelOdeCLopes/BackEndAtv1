a5ee7080064132b11c13ec2d57e3f7e703ab2c35
RafaelOdeCLopes <152555858+RafaelOdeCLopes@users.noreply.github.com>
2025-03-17 21:30:32 -0300

    Adicionar arquivos de projeto.

---
+++ BackEndAtv1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BackEndAtv1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
